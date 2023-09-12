33025224a8c185a1feb3a54449f93ab5c297326b
khajats <khajats@testvagrant.com>
2023-09-12 10:35:14 +0530

    added the other supporting files

---
+++ src/test/java/com/testvagrant/ApiValidationn/ApiValidationnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testvagrant.ApiValidationn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiValidationnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
